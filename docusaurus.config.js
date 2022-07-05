960dfd1e10c434111fde3102ad05aedaf360658f
Peter Kieltyka <peter.kieltyka@gmail.com>
2022-07-05 15:57:46 -0400

    wip

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
           items: [
             {
               type: 'doc',
-              docId: 'introduction/overview',
+              docId: 'intro/overview',
               position: 'left',
               label: 'Overview',
             },
